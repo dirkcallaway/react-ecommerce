77d9f1c33327ed7688890b7415ed2a0d8aa70ec1
M <P>
2023-11-10 15:42:47 -0700

    Added initial containers for store

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,8 +1,53 @@
 
 const App = () => {
   return (
-    <div>
-      Hello World
+    <div
+      className="categories-container">
+      <div
+        className="category-container">
+        {/* <img /> */}
+        <div
+          className="category-body-container">
+          <h2>Hats</h2>
+          Shop Now
+        </div>
+      </div>
+      <div
+        className="category-container">
+        {/* <img /> */}
+        <div
+          className="category-body-container">
+          <h2>Jackets</h2>
+          Shop Now
+        </div>
+      </div>
+      <div
+        className="category-container">
+        {/* <img /> */}
+        <div
+          className="category-body-container">
+          <h2>Sneakers</h2>
+          Shop Now
+        </div>
+      </div>
+      <div
+        className="category-container">
+        {/* <img /> */}
+        <div
+          className="category-body-container">
+          <h2>Womens</h2>
+          Shop Now
+        </div>
+      </div>
+      <div
+        className="category-container">
+        {/* <img /> */}
+        <div
+          className="category-body-container">
+          <h2>Mens</h2>
+          Shop Now
+        </div>
+      </div>
     </div>
   );
 }
